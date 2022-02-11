8323462eb0c72619596c7bd3c04fd44fc2b0376b
Ambre Gorias <ambre.gorias@gmail.com>
2022-02-10 14:05:51 -1000

    add delete and modify

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -29,3 +29,30 @@ exports.getOneSauce = (req, res, next) => {
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({error}));
 };
+
+exports.deleteSauce = (req, res, next) => {
+  Sauce.findOne({_id: req.params.id})
+    .then( sauce => {
+      if (!sauce) {
+        return res.status(404).json({ error: new Error('Sauce non trouvée') });
+      }
+      if (sauce.userId !== req.auth.userId) {
+        return res.status(403).json({ error : new Error('unauthorized request')});
+      }
+      Sauce.deleteOne({_id: req.params.id})
+        .then(() => res.status(200).json({ message: 'Sauce supprimée' }))
+        .catch(error => res.status(404).json({ error }));
+    })
+    .catch(error => res.status(500).json({error}));
+};
+
+exports.modifySauce = (req, res, next) => {
+  const sauceObject = req.file ?
+  {
+    ...JSON.parse(req.body.sauce),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+   } : {...req.body};
+  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Sauce modifiée !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -8,8 +8,8 @@ const multer = require('../middleware/multer-config');
 router.get('/', auth, sauceCtrl.getAllSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
-// router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-// router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 // router.post('/:id/like', auth, sauceCtrl.likeSauce);
 
 module.exports = router;
