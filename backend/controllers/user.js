97a4f4785ef950ba82336735d3f1e8739a82fccf
Ambre Gorias <ambre.gorias@gmail.com>
2022-02-19 19:12:52 -1000

    fix error display

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 
 const User = require("../models/User");
 
@@ -15,7 +15,12 @@ exports.signup = (req, res, next) => {
       user
         .save()
         .then(() => res.status(201).json({ message: "Utilisateur créé!" }))
-        .catch((error) => res.status(400).json({ error }));
+        .catch((error) =>
+          res.status(400).json({
+            message: "Utilisateur déjà enregistré!",
+            error: error,
+          })
+        );
     })
     .catch((error) => res.status(500).json({ error }));
 };
@@ -23,9 +28,10 @@ exports.signup = (req, res, next) => {
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     // Ici, même si l'utilisateur n'est pas trouvé, mongoose renvoit quand même une promise résolue
-    .then(user => {
+    .then((user) => {
       if (!user) {
-        return res.status(401).json({ error: "Utilisateur non trouvé!" });
+        const error = new Error("Utilisateur inconnu!");
+        return res.status(401).json({ message: error.message });
       }
       bcrypt
         .compare(req.body.password, user.password)
@@ -37,7 +43,7 @@ exports.login = (req, res, next) => {
             userId: user._id,
             // .sign ({payload = données à encoder},{clé d'encodage},{configuration})
             token: jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {
-              expiresIn: "24h",
+              expiresIn: process.env.TOKEN_EXPIRES_IN,
             }),
           });
         })
