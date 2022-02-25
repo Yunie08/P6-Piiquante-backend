f762cf3d962a634e3936f1deceb27c6d33d64b4a
Ambre Gorias <ambre.gorias@gmail.com>
2022-02-25 13:18:19 -1000

    refactor routes

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -6,11 +6,24 @@ const sauceCtrl = require('../controllers/sauce');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-router.get('/', auth, sauceCtrl.getAllSauce);
-router.get('/:id', auth, sauceCtrl.getOneSauce);
-router.post('/', auth, multer, sauceCtrl.createSauce);
-router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-router.delete('/:id', auth, sauceCtrl.deleteSauce);
-router.post('/:id/like', auth, sauceCtrl.likeSauce);
+router
+  .route('/')
+  .get(auth, sauceCtrl.getAllSauce)
+  .post(auth, multer, sauceCtrl.createSauce);
+
+router
+  .route('/:id')
+  .get(auth, sauceCtrl.getOneSauce)
+  .put(auth, multer, sauceCtrl.modifySauce)
+  .delete(auth, sauceCtrl.deleteSauce);
+
+router.route('/:id/like').post(auth, sauceCtrl.likeSauce);
+
+// router.get('/', auth, sauceCtrl.getAllSauce);
+// router.get('/:id', auth, sauceCtrl.getOneSauce);
+// router.post('/', auth, multer, sauceCtrl.createSauce);
+// router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+// router.delete('/:id', auth, sauceCtrl.deleteSauce);
+// router.post('/:id/like', auth, sauceCtrl.likeSauce);
 
 module.exports = router;
