2cacf9411946f295d67d6d49b33a9ef2ffd8fef4
Ambre Gorias <ambre.gorias@gmail.com>
2022-02-26 21:17:06 -1000

    clean up

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -19,11 +19,4 @@ router
 
 router.route('/:id/like').post(auth, sauceCtrl.likeSauce);
 
-// router.get('/', auth, sauceCtrl.getAllSauce);
-// router.get('/:id', auth, sauceCtrl.getOneSauce);
-// router.post('/', auth, multer, sauceCtrl.createSauce);
-// router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-// router.delete('/:id', auth, sauceCtrl.deleteSauce);
-// router.post('/:id/like', auth, sauceCtrl.likeSauce);
-
 module.exports = router;
