acc6b6442b0d7811d01ce9d87f8f8f851cf15a9b
Ambre Gorias <ambre.gorias@gmail.com>
2022-03-01 21:41:54 -1000

    update .env

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
@@ -14,13 +15,11 @@ const app = express();
 app.use(helmet());
 
 // Connection to database
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DB_PASSWORD
-).replace('<USER>', process.env.DB_USER);
-
 mongoose
-  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nksbe.mongodb.net/piiquante?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
@@ -38,18 +37,20 @@ app.use((req, res, next) => {
   next();
 });
 
-const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 hour
-  max: 100, // Limit each IP to 100 requests per `window`, here 1 hour
-  message: 'Too many request from this IP, please try again in an hour',
-});
-
 app.use(express.json());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
+// Data sanitization against XSS attacks
+app.use(xss());
+
 // Limit repeated requests from same id
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 150, // Limit each IP to 150 requests per `window`, here 1 hour
+  message: 'Too many request from this IP, please try again in an hour',
+});
 app.use(limiter);
 
 // Middleware declaration
